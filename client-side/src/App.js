48345c8ca3cf07ac98c897f4f5693c82a2b30050
naiem <naiem4288@gamil.com>
2020-11-17 04:45:36 +0600

    add router

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,12 +1,49 @@
 import logo from './logo.svg';
 import './App.scss';
 import HomeDetails from './components/HomeDetails/HomeDetails';
+import Login from './components/Login/Login';
+import AddHouseRent from './components/Dashboard/AddHouseRent/AddHouseRent'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Dashboard from './components/Dashboard/Dashboard/Dashboard';
+import MyRent from './components/Dashboard/MyRent/MyRent';
+import BookingList from './components/Dashboard/BookingList/BookingList';
+
 
 function App() {
   return (
-    <div className="App">
-      <HomeDetails/>
-    </div>
+    <Router>
+      
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <HomeDetails></HomeDetails>
+          </Route>
+          <Route path="/homedetails">
+            <HomeDetails></HomeDetails>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/dashboard/bookinglist">
+            <Dashboard></Dashboard>
+          </Route>
+          <Route path="/dashboard/bookinglist">
+            <BookingList></BookingList>
+          </Route>
+          <Route path="/dashboard/myrent">
+            <MyRent></MyRent>
+          </Route>
+          <Route path="/dashboard/addhouserent">
+            <AddHouseRent></AddHouseRent>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client-side/src/Components/Dashboard/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MyRent from '../MyRent/MyRent';
+import BookingList from '../BookingList/BookingList';
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      
+      <BookingList></BookingList>
+    </div>
+  );
+};
+
+export default Dashboard;
